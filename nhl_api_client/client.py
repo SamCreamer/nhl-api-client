4b28749d63fd061f5507a5af32c7e4ae8c298cec
Scott Luu <scottluu95@gmail.com>
2021-06-09 23:20:28 -0400

    use requests session

--- nhl_api_client/client.py
+++ nhl_api_client/client.py
@@ -1,5 +1,15 @@
+from typing import Optional
+
 import requests
 
 
 class NHLApiClient:
-    ...
+    _http_session: Optional[requests.Session] = None
+
+    def __enter__(self):
+        with requests.Session() as session:
+            self._http_session = session
+            yield self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
